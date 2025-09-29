dc5373bf9a25361dd80f2e4852bd221d35e4ace5
SnowyWalk <chunsa1166jp@gmail.com>
2025-09-30 00:15:57 +0900

    서버 컴파일 에러 수정

--- server/Controller/AdminController.cs
+++ server/Controller/AdminController.cs
@@ -10,44 +10,35 @@ public class AdminController : ControllerBase
 {
     private readonly Database m_database;
 
-    public class WorldCategoryDTO
-    {
-        public string WorldId { get; set; }
-        public List<WorldCategory> CategoryList { get; set; }
-    }
-
     public AdminController(Database database)
     {
         m_database = database;
     }
 
     [HttpGet("getcategory/{worldIdListString}")]
-    public async Task<ActionResult<List<WorldCategoryDTO>>> GetWorldDataCategoryList(string worldIdListString)
+    public async Task<ActionResult<Dictionary<string,List<WorldCategory>>>> GetWorldDataCategoryList(string worldIdListString)
     {
         string[] worldIdList = worldIdListString.Split('^').ToArray();
-        List<WorldCategory> result = await m_database.GetWorldDataCategoryList(worldIdList);
+        Dictionary<string,List<WorldCategory>> result = await m_database.GetWorldDataCategoryList(worldIdList);
         return Ok(result);
     }
 
     [HttpPost("setcategory/{worldId}/{categoryListString}")]
-    public async Task<ActionResult<WorldCategoryDTO>> UpdateWorldDataCategoryList(string worldId, string categoryListString)
+    public async Task<ActionResult<Dictionary<string, List<WorldCategory>>>> UpdateWorldDataCategoryList(string worldId, string categoryListString)
     {
         var categoryIdList = categoryListString.Split('^').ToList();
-        List<WorldCategory> resultCategoryList;
         try
         {
             await m_database.UpdateWorldDataCategoryList(worldId, categoryIdList);
-
         }
         catch (Exception e)
         {
             return StatusCode(500, e);
         }
+        List<WorldCategory> finalState = await m_database.GetWorldDataCategoryList(worldId);
+        Dictionary<string, List<WorldCategory>> result = new();
+        result[worldId] = finalState;
 
-        return Ok(new WorldCategoryDTO()
-        {
-            WorldId = worldId,
-            CategoryList = (await GetWorldDataCategoryList(worldId)).Result,
-        });
+        return Ok(result);
     }
 }
